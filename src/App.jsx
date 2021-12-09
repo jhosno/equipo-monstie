4098445f61440b474ef385808ad72192e8436a27
jhosno <jhosnoirlit@gmail.com>
2021-12-08 20:43:35 -0400

    UPDATE: littles changes & add form

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react';
 import "tailwindcss/tailwind.css";
 import './App.css';
+import Form from './components/Form';
 import Header from './components/Header';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App container">
+    <div className="App container h-screen">
       <Header/>
+      <Form/>
 
     </div>
   )
