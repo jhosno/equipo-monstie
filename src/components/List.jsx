d065a4b3516a225e8d59782b12a8a90d44fcc557
jhosno <jhosnoirlit@gmail.com>
2021-12-20 20:10:11 -0400

    CREATE: list

---
+++ src/components/List.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function List({monsties}) {
+  return (
+    <div className="md:w-1/2 lg:w-3/5 mb-5">
+      <h2 className="text-green-700 font-bold text-center text-xl  p-5">
+        Estos son los monsties de tu equipo
+      </h2>
+
+    </div>
+  );
+}
+
+export default List;
