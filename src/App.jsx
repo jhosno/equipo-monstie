403dcb0ea2e94054351b9b56117683d5aafa8e5d
jhosno <jhosnoirlit@gmail.com>
2022-01-04 15:03:46 -0400

    UPDATE: localsotre

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import "tailwindcss/tailwind.css";
 import './App.css';
 import Form from './components/Form';
@@ -10,6 +10,31 @@ import List from './components/List';
 function App() {
   const [count, setCount] = useState(0)
   const [monsties, setmonsties] = useState([])
+  const [monstie, setmonstie] = useState({})
+
+//Carga los registros almacenados en localStore
+useEffect(()=>{
+  const getLocalStore = ()=>{
+    const monstieLocalStore = JSON.parse(localStorage.getItem('monsties'))
+
+    setmonsties(monstieLocalStore)
+  }
+  getLocalStore()
+}, [])
+
+//Escribe y actualiza el localStore
+useEffect(()=>{
+  localStorage.setItem('monsties', JSON.stringify(monsties))
+
+}, [monsties])
+
+const deleteMonstie = (id) =>{
+    const monstieAfterDelete = monsties.filter(
+      monstie=> monstie.id !== id
+    )
+    setmonsties(monstieAfterDelete)
+  }
+  
 
   return (
     <div className="App container h-screen">
@@ -18,9 +43,13 @@ function App() {
         <Form
           monsties = {monsties}
           setmonsties = {setmonsties}
+          monstie = {monstie}
+          setmonstie = {setmonstie}
         />
         <List
           monsties = {monsties}
+          setmonstie = {setmonstie}
+          deleteMonstie = {deleteMonstie}
         />
       </div>
 
