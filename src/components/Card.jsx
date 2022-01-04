ab7368d9f3cd8b4ccb73f48662f0146ab5e1b775
jhosno <jhosnoirlit@gmail.com>
2022-01-04 15:03:29 -0400

    UPDATE: set edit functions

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
-function Card({monstie}) {
-    const {nick, breed, rarity, attack} = monstie
-    const HandleEdit = (e) =>{
+function Card({monstie, setmonstie, deleteMonstie}) {
+    const {nick, breed, rarity, trends, id} = monstie;
+    const handleDelete = ()=>{
+      const answer = confirm("¿Quieres remover este monstie de tu equipo?");
 
+      if(answer){
+        deleteMonstie(id)
+      }
     }
+    
   return (
     <div className="bg-gray-50 p-8 m-5 ml-10 rounded-2xl">
       <p className="text-green-700 font-black  text-lg tracking-wide uppercase">
@@ -21,16 +26,18 @@ function Card({monstie}) {
       </p>
       <p className="text-green-700 font-black  text-lg tracking-wide uppercase">
         Ataque:{" "}
-        <span className="text-gray-800 font-semibold capitalize ">{attack}</span>
+        <span className="text-gray-800 font-semibold capitalize ">{trends}</span>
       </p>
       <div className="flex justify-between mt-4">
         <button 
           className="cursor-pointer py-3 px-5 bg-green-800 text-green-100 uppercase font-bold tracking-wide rounded-md hover:bg-green-100 hover:text-green-800"
-          onClick={HandleEdit}
+          onClick={() => setmonstie(monstie)}
           >
           ✏ Edit
         </button>
-        <button className="cursor-pointer py-3 px-5  text-red-600 uppercase font-bold tracking-wide rounded-md hover:bg-red-600 hover:text-red-100">
+        <button className="cursor-pointer py-3 px-5  text-red-600 uppercase font-bold tracking-wide rounded-md hover:bg-red-600 hover:text-red-100"
+          onClick={handleDelete}
+        >
           🗑 Delete
         </button>
       </div>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,49 +1,81 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import Errors from "./Errors";
 
+const Form = ({ monsties, setmonsties, monstie, setmonstie }) => {
+  const [nick, setnick] = useState("");
+  const [breed, setbreed] = useState("");
+  const [rarity, setrarity] = useState("");
+  const [trends, settrends] = useState("");
 
-const Form = ({monsties, setmonsties}) => {
-    const [nick, setnick] = useState("")
-    const [breed, setbreed] = useState("")
-    const [rarity, setrarity] = useState("")
-    const [trends, settrends] = useState("")
-    const [error, seterror] = useState(false)
-
-    //handleSubmit recibe la variable e/event que es el resultado del form
-    const handleSubmit= (e) =>{
-      //para evitar que la página se recargué al anviar el form
-      e.preventDefault()
-      
-      const addMonstie = {
-        nick,
-        breed,
-        rarity,
-        trends
-      }
-        //ahora preguntamos sí los campos están vacíos
-        //Para eso usamos las variables de useState que representan cada uno de los inputs
-        if([nick, breed, rarity, trends].includes('')){
-            //los metemos en un array y con la function includes, preguntamos si hay alhuna variable vacía
-            
-            //Ahora que ya validamos debemos mostrarle al usuario que faltana campos
-            seterror(true)
-            return
-        }else{
-            console.log('perfect!')
-            seterror(false)
-            //Agregamos el nuevo objeto con los valores del monstie
-            setmonsties([...monsties, addMonstie])
-
-        }
+  const [error, seterror] = useState(false);
+  useEffect(() => {
+    if (Object.keys(monstie).length > 0) {
+      setnick(monstie.nick);
+      setbreed(monstie.breed);
+      setrarity(monstie.rarity);
+      settrends(monstie.trends);
     }
+  }, [monstie]);
+  //generar ID único
+  const idGenerator = () => {
+    return Math.random().toString(36).substr(2) + Date.now().toString(36);
+  };
+
+  //handleSubmit recibe la variable e/event que es el resultado del form
+  const handleSubmit = (e) => {
+    //para evitar que la página se recargué al anviar el form
+    e.preventDefault();
+
+    const addMonstie = {
+      nick,
+      breed,
+      rarity,
+      trends,
+    };
+    //ahora preguntamos sí los campos están vacíos
+    //Para eso usamos las variables de useState que representan cada uno de los inputs
+    if ([nick, breed, rarity, trends].includes("")) {
+      //los metemos en un array y con la function includes, preguntamos si hay alhuna variable vacía
+
+      //Ahora que ya validamos debemos mostrarle al usuario que faltana campos
+      seterror(true);
+      return;
+    }
+    seterror(false);
+
+    if (monstie.id) {
+      //Edit
+      console.log("Update");
+      addMonstie.id = monstie.id;
+      const updateMonstie = monsties.map((monstieState) =>
+        monstieState.id === addMonstie.id ? addMonstie : monstieState
+      );
+      setmonsties(updateMonstie);
+      setmonstie({});
+    } else {
+      //Create
+      console.log("Add");
+      addMonstie.id = idGenerator();
+      //Agregamos el nuevo objeto con los valores del monstie
+      setmonsties([...monsties, addMonstie]);
+    }
+
+    //resetea el form
+    setnick("");
+    setbreed("");
+    setrarity("");
+    settrends("");
+  };
   return (
+    
     <div className="md:w-1/2 lg:w-2/5 mb-5">
       <h2 className="text-green-700 font-bold text-center text-xl  p-5">
         Agrega tu nuevo monstie
       </h2>
-      <form className="bg-gray-50 p-8 m-5 ml-10 rounded-2xl"
-      //sin parentesis porque entonces llamas directamente la función y  se va a ejecutar automáticamente a cada rato 
-      onSubmit={handleSubmit}
+      <form
+        className="bg-gray-50 p-8 m-5 ml-10 rounded-2xl"
+        //sin parentesis porque entonces llamas directamente la función y  se va a ejecutar automáticamente a cada rato
+        onSubmit={handleSubmit}
       >
         <div className="my-6">
           <label
@@ -55,6 +87,7 @@ const Form = ({monsties, setmonsties}) => {
           <input
             type="text"
             name="nick"
+            value={nick}
             id="nick"
             onChange={(e) => setnick(e.target.value)}
             className="w-full  p-2 border-2 rounded-lg"
@@ -78,8 +111,8 @@ const Form = ({monsties, setmonsties}) => {
             type="text"
             name="breed"
             id="breed"
+            value={breed}
             onChange={(e) => setbreed(e.target.value)}
-
             className="w-full  p-2 border-2 rounded-lg"
           />
         </div>
@@ -93,11 +126,11 @@ const Form = ({monsties, setmonsties}) => {
           <input
             type="number"
             min="1"
-            max = "9"
+            max="9"
             name="rarity"
+            value={rarity}
             id="rarity"
             onChange={(e) => setrarity(e.target.value)}
-
             className="w-full  p-2 border-2 rounded-lg"
           />
         </div>
@@ -109,10 +142,10 @@ const Form = ({monsties, setmonsties}) => {
             Tendencia de ataque
           </label>
           <select
-            name="trend"
-            id="trend"
+            name="trends"
+            id="trends"
+            value={trends}
             onChange={(e) => settrends(e.target.value)}
-
             className="w-full  p-2 border-2 rounded-lg"
           >
             <option value="">Selecciona una opción</option>
@@ -124,10 +157,10 @@ const Form = ({monsties, setmonsties}) => {
         {error ? <Errors message="You must be fill all inputs" /> : null}
         <div className="flex justify-items-end">
           <input
-            className="bg-green-900 rounded-md shadow-3xl border-3 border-white text-green-300 py-2 px-4 font-semibold uppercase 
+            className="bg-green-900 rounded-md shadow-3xl border-3 border-white text-green-100 py-2 px-4 font-semibold uppercase 
           hover:text-green-900 hover:bg-green-300 cursor-pointer"
             type="submit"
-            value="Agregar"
+            value={monstie.id ? "✏ Edit" : "➕ Agregar"}
           />
         </div>
       </form>
