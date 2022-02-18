924f7731e17cf495ce922a8697ac0e4d6173e61b
jhosno <jhosnoirlit@gmail.com>
2022-02-18 13:37:32 -0400

    update dist config

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { useState, useEffect } from 'react';
 import "tailwindcss/tailwind.css";
-import './App.css';
+
 import Form from './components/Form';
 import Header from './components/Header';
 import List from './components/List';
-
 
 
 function App() {
